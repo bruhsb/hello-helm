a2972c2fd99cfd103bd3c56b61bf529d339a04e2
Bruno S. Brasil <bruh-sb@live.com>
2019-07-29 15:17:42 -0300

    feat: change app message

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 /**
-* Copyright 2014 IBM
-*
-*   Licensed under the Apache License, Version 2.0 (the "License");
-*   you may not use this file except in compliance with the License.
-*   You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-*   Unless required by applicable law or agreed to in writing, software
-*   distributed under the License is distributed on an "AS IS" BASIS,
-*   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-*   See the License for the specific language governing permissions and
+// #  Copyright 2019
+// #
+// #   Licensed under the Apache License, Version 2.0 (the "License");
+// #   you may not use this file except in compliance with the License.
+// #   You may obtain a copy of the License at
+// #
+// #     http://www.apache.org/licenses/LICENSE-2.0
+// #
+// #   Unless required by applicable law or agreed to in writing, software
+// #   distributed under the License is distributed on an "AS IS" BASIS,
+// #   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// #   See the License for the specific language governing permissions and
+// #   limitations under the License.
 **/
 
 var express = require('express');
@@ -19,7 +20,7 @@ var PORT = 80;
 
 var app = express();
 app.get('/', function (req, res) {
-  res.send('Welcome to IBM Cloud DevOps with Docker, Kubernetes and Helm Charts. Lets go use the Continuous Delivery Service');
+  res.send('Hi there!! Welcome to Demo APP with Docker, Kubernetes and Helm Charts.');
 });
 
 app.listen(PORT)
